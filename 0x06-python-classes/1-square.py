68a1f58a7c5fc1f48d3c741c999c2aa5574d9859
Gbenga <tolsoft32@gmail.com>
2023-08-29 09:28:53 -0700

    Square with no complete attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defining class named Square"""
+
+class Square:
+    """the square class parent"""
+    def __init__(self, size):
+        """This initializes a new square.
+
+        Attributes:
+            size(int): initializes sizse of new square
+        """
+        self.__size = size

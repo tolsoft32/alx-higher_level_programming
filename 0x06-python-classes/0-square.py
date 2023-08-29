37d101f5cda68e79cd080da5383b1ad3ce7132f9
Gbenga <tolsoft32@gmail.com>
2023-08-29 09:15:17 -0700

    Empty Class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Square Created Class
+
+This class instance will not do anything
+"""
+
+class Square:
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+#Getting started with Python Objected Oriented programming
